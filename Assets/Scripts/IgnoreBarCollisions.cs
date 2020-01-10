ec54c530889598327f4568b7659b80fe5d7074ea
Austin Bobco <abobco@gmail.com>
2020-01-10 16:32:47 -0600

    camera smoothing, bar chart

---
+++ Assets/Scripts/IgnoreBarCollisions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreBarCollisions : MonoBehaviour
+{
+
+    SphereCollider myCollider;
+
+    void Start()
+    {
+        myCollider = gameObject.GetComponent(typeof(SphereCollider)) as SphereCollider;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "BarChart")
+        {
+            Physics.IgnoreCollision(collision.collider, myCollider);
+        }
+    }
+}
